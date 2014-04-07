585be4b235c0c723f125973c5c821e49d30eeeca
yangjeewoong <danielay@nhnnext.org>
2014-04-07 23:41:43 +0900

    stream controller - stream method fill

--- chatstream/app/controllers/stream_controller.rb
+++ chatstream/app/controllers/stream_controller.rb
@@ -1,4 +1,8 @@
 class StreamController < ApplicationController
+  include ActionController::Live
+
   def stream
+    response.headers['Content-Type'] = 'text/event-stream'
+    render nothing: true
   end
 end
