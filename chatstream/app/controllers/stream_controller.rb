581ce351eabd95ccbbc3d57f6713b1fe4922c17a
yangjeewoong <danielay@nhnnext.org>
2014-04-07 23:46:53 +0900

    Add source by using redis, stream controller - stream method

--- chatstream/app/controllers/stream_controller.rb
+++ chatstream/app/controllers/stream_controller.rb
@@ -3,6 +3,21 @@ class StreamController < ApplicationController
 
   def stream
     response.headers['Content-Type'] = 'text/event-stream'
+    
+    redis = Redis.new
+    redis.subscribe('new_message') do |on|
+      on.message do |event, data|
+        response.stream.write "event: #{event}\n"
+        response.stream.write "data: #{data}\n\n"
+        puts event, data
+      end
+    end
+
     render nothing: true
+  rescue IOError
+
+  ensure
+    redis.quit
+    response.stream.close
   end
 end
